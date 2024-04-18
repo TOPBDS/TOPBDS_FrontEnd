acb2322638c746779068e636344a07865384b6b1
yongbin1 <rhdydqls1024@naver.com>
2024-04-19 01:01:36 +0900

    sub header icon 추가

--- src/layout/header/SubHeader.tsx
+++ src/layout/header/SubHeader.tsx
@@ -1,13 +1,14 @@
 import { SubHeaderMenuItemStyle, SubHeaderMenuStyle, SubHeaderStyle } from "../style/layout.style";
+import { ReactComponent as ArrowBottom } from "../../assets/icon/arrow_bottom.svg";
 
 const SubHeader = () => {
     return (
         <SubHeaderStyle>
             <SubHeaderMenuStyle>
-                <SubHeaderMenuItemStyle>부동산 유형</SubHeaderMenuItemStyle>
-                <SubHeaderMenuItemStyle>지역 거래</SubHeaderMenuItemStyle>
-                <SubHeaderMenuItemStyle>거래 유형</SubHeaderMenuItemStyle>
-                <SubHeaderMenuItemStyle>특징별 유형</SubHeaderMenuItemStyle>
+                <SubHeaderMenuItemStyle>부동산 유형 <ArrowBottom /></SubHeaderMenuItemStyle>
+                <SubHeaderMenuItemStyle>지역 거래 <ArrowBottom /></SubHeaderMenuItemStyle>
+                <SubHeaderMenuItemStyle>거래 유형 <ArrowBottom /></SubHeaderMenuItemStyle>
+                <SubHeaderMenuItemStyle>특징별 유형 <ArrowBottom /></SubHeaderMenuItemStyle>
             </SubHeaderMenuStyle>
         </SubHeaderStyle>
     )
