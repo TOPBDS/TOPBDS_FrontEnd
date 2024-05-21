6539560c4bfdc8e7393025bbe38085ad5918e903
장석연 <jangseokyeon1110@gmail.com>
2024-05-21 12:57:35 +0900

    상세 페이지 수정 #2

--- src/components/main/style/main-item.style.ts
+++ src/components/main/style/main-item.style.ts
@@ -35,10 +35,12 @@ export const HouseDetailSortation = styled.div`
 export const HouseDetailUnsoldStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 500px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: #f0f9;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -46,15 +48,22 @@ export const HouseDetailUnsoldStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailPopulationChangeStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 500px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: #d6f6d6;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -62,15 +71,22 @@ export const HouseDetailPopulationChangeStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailRankStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: #d32f3d;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -78,15 +94,22 @@ export const HouseDetailRankStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailSupplyVolumeStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: green;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -94,15 +117,22 @@ export const HouseDetailSupplyVolumeStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailRecentlyDownFallAptStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: blue;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -110,15 +140,22 @@ export const HouseDetailRecentlyDownFallAptStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailRecentlyRiseAptStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: skyblue;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -126,15 +163,22 @@ export const HouseDetailRecentlyRiseAptStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailAptsStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: orange;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -142,15 +186,22 @@ export const HouseDetailAptsStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailAptsPriceStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: grey;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -158,15 +209,22 @@ export const HouseDetailAptsPriceStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
 
 export const HouseDetailAptReviewStyle = styled.div`
     box-sizing: border-box;
 
-    width: 410px;
-    height: 600px;
+    width: 25.625rem;
+    height: 100%;
 
     background-color: #fff;
+
+    padding: 1.5rem;
 
     border-right: 1px solid #D5D7DD;
     border-bottom: 1px solid #D5D7DD;
@@ -174,4 +232,9 @@ export const HouseDetailAptReviewStyle = styled.div`
     flex: none;
     order: 0;
     flex-grow: 0;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1.5rem;
 `
