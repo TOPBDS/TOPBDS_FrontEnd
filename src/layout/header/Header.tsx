1f5a98c7de0e7e063f985488d0486da8b1f22318
장석연 <jangseokyeon1110@gmail.com>
2024-06-03 00:06:20 +0900

    헤더 수정

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
             <a href="/"><LogoImageStyle src={LOGO} /></a>
             <HeaderMenuStyle>
                 <HeaderMenuItemStyle><a href="/blog">블로그</a></HeaderMenuItemStyle>
-                <HeaderMenuItemStyle><a href="/search">매물 검색</a></HeaderMenuItemStyle>
                 <HeaderMenuItemStyle><a href="/report">보고서</a></HeaderMenuItemStyle>
                 <HeaderMenuItemStyle><a href="/my">마이페이지</a></HeaderMenuItemStyle>
             </HeaderMenuStyle>
