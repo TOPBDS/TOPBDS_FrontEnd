64a3ea8aebf8d1bd5fdfb906674f0dab8e47d2a4
yongbin1 <rhdydqls1024@naver.com>
2024-06-03 09:10:38 +0900

    블로그, 보고서 이동 방지 (구현 x)

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -6,8 +6,8 @@ const Header = () => {
         <HeaderStyle>
             <a href="/"><LogoImageStyle src={LOGO} /></a>
             <HeaderMenuStyle>
-                <HeaderMenuItemStyle><a href="/blog">블로그</a></HeaderMenuItemStyle>
-                <HeaderMenuItemStyle><a href="/report">보고서</a></HeaderMenuItemStyle>
+                <HeaderMenuItemStyle><a href="/">블로그</a></HeaderMenuItemStyle>
+                <HeaderMenuItemStyle><a href="/">보고서</a></HeaderMenuItemStyle>
                 <HeaderMenuItemStyle><a href="/login">로그인</a></HeaderMenuItemStyle>
             </HeaderMenuStyle>
         </HeaderStyle>
