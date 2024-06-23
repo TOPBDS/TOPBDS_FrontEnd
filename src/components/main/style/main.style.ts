bcd315552af4ee9b3abdff8ae1b55ea88e04d374
장석연 <jangseokyeon1110@gmail.com>
2024-06-23 21:54:38 +0900

    풀 받기

--- src/components/main/style/main.style.ts
+++ src/components/main/style/main.style.ts
@@ -92,7 +92,12 @@ export const HouseSearchIcon = styled.button`
 export const HouseListItemStyle = styled.div`
     box-sizing: border-box;
 
+<<<<<<< Updated upstream
     width: 25.625rem;
+=======
+    width: 410px;
+    height: 100%;
+>>>>>>> Stashed changes
 
     border-bottom: 1px solid #D5D7DD;
 
