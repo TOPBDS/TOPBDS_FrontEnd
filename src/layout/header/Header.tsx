64cc4ed4cc2758475390facdefe2e9eb39b5004a
yongbin1 <rhdydqls1024@naver.com>
2024-06-24 17:53:10 +0900

    지도 동작 수정 및 현재 위치 가져오기

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,21 +2,23 @@ import { HeaderMenuItemStyle, HeaderMenuStyle, HeaderStyle } from "../style/layo
 import { ReactComponent as SLOGO } from "../../assets/icon/s-logo.svg";
 import "../style/layout.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
     const [ isLogin, setIsLogin ] = useState<boolean>(true);
 
     return (
         <HeaderStyle>
             <a href="/"><SLOGO className="logo" /></a>
             <HeaderMenuStyle>
-                <HeaderMenuItemStyle><a href="/">블로그</a></HeaderMenuItemStyle>
-                <HeaderMenuItemStyle><a href="/">보고서</a></HeaderMenuItemStyle>
+                <HeaderMenuItemStyle onClick={() => navigate("/")}>블로그</HeaderMenuItemStyle>
+                <HeaderMenuItemStyle onClick={() => navigate("/")}>보고서</HeaderMenuItemStyle>
                 {
                     isLogin ? (
-                        <HeaderMenuItemStyle><a href="/my">마이페이지</a></HeaderMenuItemStyle>
+                        <HeaderMenuItemStyle onClick={() => navigate("my")}>마이페이지</HeaderMenuItemStyle>
                     ) : (
-                        <HeaderMenuItemStyle><a href="/login">로그인</a></HeaderMenuItemStyle>
+                        <HeaderMenuItemStyle onClick={() => navigate("login")}>로그인</HeaderMenuItemStyle>
                     )
                 }
             </HeaderMenuStyle>
