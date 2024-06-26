ad5538cf87d145f71ec6fbb1c618625828447210
yongbin1 <rhdydqls1024@naver.com>
2024-06-27 00:52:59 +0900

    header 아이콘 추가 및 login 페이지 z-index 수정

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,8 @@
 import { HeaderMenuItemStyle, HeaderMenuStyle, HeaderStyle } from "../style/layout.style";
 import { ReactComponent as SLOGO } from "../../assets/icon/s-logo.svg";
+import { ReactComponent as BLOG } from "../../assets/icon/blog.svg";
+import { ReactComponent as REPORT } from "../../assets/icon/report.svg";
+import { ReactComponent as PROFILE } from "../../assets/icon/profile.svg";
 import "../style/layout.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -12,11 +15,11 @@ const Header = () => {
         <HeaderStyle>
             <a href="/"><SLOGO className="logo" /></a>
             <HeaderMenuStyle>
-                <HeaderMenuItemStyle onClick={() => navigate("/blog")}>블로그</HeaderMenuItemStyle>
-                <HeaderMenuItemStyle onClick={() => navigate("/")}>보고서</HeaderMenuItemStyle>
+                <HeaderMenuItemStyle onClick={() => navigate("/blog")}><BLOG /></HeaderMenuItemStyle>
+                <HeaderMenuItemStyle onClick={() => navigate("/")}><REPORT /></HeaderMenuItemStyle>
                 {
                     isLogin ? (
-                        <HeaderMenuItemStyle onClick={() => navigate("my")}>마이페이지</HeaderMenuItemStyle>
+                        <HeaderMenuItemStyle onClick={() => navigate("my")}><PROFILE /></HeaderMenuItemStyle>
                     ) : (
                         <HeaderMenuItemStyle onClick={() => navigate("login")}>로그인</HeaderMenuItemStyle>
                     )
--- src/pages/auth/login/style/login.style.ts
+++ src/pages/auth/login/style/login.style.ts
@@ -9,6 +9,8 @@ export const LoginContainer = styled.div`
 
     background: rgba(0, 0, 0, 0.3);
 
+    z-index: 999;
+
     display: flex;
     justify-content: center;
     align-items: center;
