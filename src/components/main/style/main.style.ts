50b987336ec6d50ac01e385646b5b2d17debd391
yongbin1 <rhdydqls1024@naver.com>
2024-08-26 01:29:48 +0900

    valuelabel css 추가

--- src/components/main/style/main.style.ts
+++ src/components/main/style/main.style.ts
@@ -301,7 +301,7 @@ export const CustomSlider = styled(Slider)({
     color: '#52af77', // 슬라이더 트랙과 thumb 색상
     height: 8, // 슬라이더 높이
 
-    '& .MuiSlider-thumb': {
+    '& .MuiSlider-thumb': { // 슬라이더 썸(이동 가능한 점)에 대한 스타일을 지정합니다.
         height: 24,
         width: 24,
         backgroundColor: '#fff',
@@ -309,26 +309,40 @@ export const CustomSlider = styled(Slider)({
         marginTop: -8,
         marginLeft: -12,
         '&:focus, &:hover, &.Mui-active': {
-        boxShadow: 'inherit',
+            boxShadow: 'inherit',
         },
     },
 
-    '& .MuiSlider-track': {
+    '& .MuiSlider-track': { // 선택된 범위에 대한 트랙 스타일을 지정합니다.
         border: 'none',
     },
 
-    '& .MuiSlider-rail': {
+    '& .MuiSlider-rail': { // 선택되지 않은 범위에 대한 레일 스타일을 지정합니다.
         opacity: 0.5,
         backgroundColor: '#bfbfbf',
     },
 
-    '& .MuiSlider-mark': {
+    '& .MuiSlider-mark': { // 슬라이더 마크(점) 스타일을 지정합니다.
         backgroundColor: '#bfbfbf',
         height: 8,
         width: 1,
         '&.MuiSlider-markActive': {
-        opacity: 1,
-        backgroundColor: 'currentColor',
+            opacity: 1,
+            backgroundColor: 'currentColor',
+            color: '#fff'
+        },
+    },
+
+    '& .MuiSlider-valueLabel': { // valueLabel에 대한 스타일을 지정합니다.
+        backgroundColor: '#52af77',
+        color: '#fff',
+        borderRadius: '4px',
+        top: -10,
+        '&:before': {
+            transform: 'rotate(45deg)',
+            width: 8, 
+            height: 8,
+            backgroundColor: '#52af77',
         },
     },
 });
